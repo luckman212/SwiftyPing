da1bc80e7bc6039baab5e7c33e9e162d481d3dde
luckman212 <1992842+luckman212@users.noreply.github.com>
2025-03-22 15:00:49 -0400

    update

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     platforms: [
         .macOS(.v12)
     ],
-    platforms: [
-        .macOS(.v12)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
